1bef954cdac9148afaf994ee2282cb0b3210528d
ToxaKartoxa <toxadizel@yandex.ru>
2025-05-26 06:47:06 +0500

    ковыряю..

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from router import router as tasks_router, user as user_router
 
 @asynccontextmanager        # декоратор, позволяет создавать контекстные менеджеры
 async def lifespan(app: FastAPI):
-    # await delete_tables()   # сначала дропаются все старые таблицы
-    # print("База очищена")
-    # await create_tables()   # асинхронное взаимодействие с базой
+    await delete_tables()   # сначала дропаются все старые таблицы
+    print("База очищена")
+    await create_tables()   # асинхронное взаимодействие с базой
     print("База готова к работе")
     yield
     print("Выключение")
