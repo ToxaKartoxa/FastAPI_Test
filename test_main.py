3e64ae9a21fb2aa9ac65f26dbbb1249d45808491
ToxaKartoxa <toxadizel@yandex.ru>
2025-06-03 20:15:47 +0500

    первая fuc записывает, вторая проверяет

--- test_main.py
+++ test_main.py
@@ -1,29 +1,42 @@
+import json
+
 from fastapi.testclient import TestClient
 
 from main import app
 
 client = TestClient(app)
 
+task_id = 0
 
 def test_create_task():
     response = client.post(
+#        "/tasks?name=000&description=0000" #,
         "/tasks",
-        json={"name": "000", "description": "0000"},
+        json={
+            "name": "000",
+            "description": "0000"
+        },
     )
     assert response.status_code == 200
-    assert response.json() == {
-        "id": 1,
-        "name": "000",
-        "description": "0000",
-    }
+    # assert response.json() == {
+    #     'ok': True,
+    #     'task_id': 8
+    # }
+    jsn = response.json()
+    assert jsn.get("task_id")
+    assert jsn.get("ok")
+    global task_id
+    task_id = jsn.get("task_id")
+
 
 def test_read_task():
-    response = client.get("/tasks/id/1")
+    global task_id
+    response = client.get("/tasks/id/" + str(task_id))
     assert response.status_code == 200
     assert response.json() == {
-        "id": 1,
-        "name": "111",
-        "description": "1111",
+        "id": task_id,
+        "name": "000",
+        "description": "0000",
     }
 
 # def test_read_nonexistent_item():
